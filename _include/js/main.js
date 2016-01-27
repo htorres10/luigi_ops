2783937e491a34215da0aaba7bb9d92705d66026
flatoveloz <cristianveraza@gmail.com>
2016-01-27 01:57:59 -0300

    Bla

--- _include/js/main.js
+++ _include/js/main.js
@@ -80,8 +80,8 @@ BRUSHED.slider = function(){
 											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content"></div>', thumb : '', url : ''},
 											{image : '_include/img/slider-images/image02.jpg', title : '<div class="slide-content"></div>', thumb : '', url : ''},
 											{image : '_include/img/slider-images/image03.jpg', title : '<div class="slide-content"></div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content"></div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image05.jpg', title : '<div class="slide-content"></div>', thumb : '', url : ''}  
+-											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content"></div>', thumb : '', url : ''},
+-											{image : '_include/img/slider-images/image05.jpg', title : '<div class="slide-content"></div>', thumb : '', url : ''}    
 									],
 									
 		// Theme Options			   
@@ -205,56 +205,6 @@ BRUSHED.filterZapatos = function (){
 	}
 }
 
-/* ==================================================
-   Filter Accesorios
-================================================== */
-
-BRUSHED.filterZapatos = function (){
-	if($('#projectsacc').length > 0){		
-		var $container = $('#projectsacc');
-		
-		$container.imagesLoaded(function() {
-			$container.isotope({
-			  // options
-			  animationEngine: 'best-available',
-			  itemSelector : '.item-thumbs',
-			  layoutMode : 'fitRows'
-			});
-		});
-	
-		// filter items when filter link is clicked
-		var $optionSets = $('#optionsacc .option-set'),
-			$optionLinks = $optionSets.find('a');
-	
-		  $optionLinks.click(function(){
-			var $this = $(this);
-			// don't proceed if already selected
-			if ( $this.hasClass('selected') ) {
-			  return false;
-			}
-			var $optionSet = $this.parents('.option-set');
-			$optionSet.find('.selected').removeClass('selected');
-			$this.addClass('selected');
-	  
-			// make option object dynamically, i.e. { filter: '.my-filter-class' }
-			var options = {},
-				key = $optionSet.attr('data-option-key'),
-				value = $this.attr('data-option-value');
-			// parse 'false' as false boolean
-			value = value === 'false' ? false : value;
-			options[ key ] = value;
-			if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-			  // changes in layout modes need extra logic
-			  changeLayoutMode( $this, options )
-			} else {
-			  // otherwise, apply new options
-			  $container.isotope( options );
-			}
-			
-			return false;
-		});
-	}
-}
 
 /* ==================================================
    FancyBox
@@ -266,8 +216,8 @@ BRUSHED.fancyBox = function(){
 		$(".fancybox").fancybox({				
 				padding : 0,
 				beforeShow: function () {
-					//Saque por falta de informacion//this.title = $(this.element).attr('title');
-					//Saque por falta de informacion//this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
+					this.title = $(this.element).attr('title');
+					//this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
 				},
 				helpers : {
 					title : { type: 'inside' },
